ff5f233567958a10bed1fcce047f3b20a8e94b0a
Benji Miles <benjimiles1999@gmail.com>
2023-03-14 06:55:23 +0200

    Added the code

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
 # 1. If the input is divisible by 3, the function will return the string "Fizz".
 # 2. If the input is divisible by 5 it will return "Buzz".
 # 3. If the input is divisible by both 3 and 5 it will return "FizzBuzz".
-# 4. For any other number it will return the same input.a
+# 4. For any other number it will return the same input.
+
+def fizz_Buzz(input):
+    if input % 3 and input % 5 == 0:
+        return "FizzBuzz"
+    if input % 3 == 0:
+        return "Fizz"
+    if input % 5 == 0:
+        return "Buzz"
+    return input
+
+print(fizz_Buzz(15))
