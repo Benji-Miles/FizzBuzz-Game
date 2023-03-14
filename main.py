599eac4aae37b142a66d4305ac52aabd3181ec7f
Benji Miles <benjimiles1999@gmail.com>
2023-03-14 06:47:43 +0200

    Added comment explanations

---
+++ main.py
@@ -0,0 +1,4 @@
+# 1. If the input is divisible by 3, the function will return the string "Fizz".
+# 2. If the input is divisible by 5 it will return "Buzz".
+# 3. If the input is divisible by both 3 and 5 it will return "FizzBuzz".
+# 4. For any other number it will return the same input.a
